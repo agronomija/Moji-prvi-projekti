0c7666512f7bb1a26b59b63f49f8bd3f02c23c7e
Andraz Corel <serviranje@gmail.com>
2021-06-10 18:59:07 +0200

    gesla popravljena na csv shranjevanje

--- gesla/gesla2.py
+++ gesla/gesla2.py
@@ -20,12 +20,17 @@ def shrani():
     email = dlg.leEmail.text()
     geslo = dlg.leGeslo.text()
 
-    slovar_gesel[ime_app] = [uporabnisko_ime, email, geslo]
+    with open('gesla.csv', 'a', encoding='utf-8') as datoteka:
+        datoteka.write(f'{ime_app},{uporabnisko_ime},{email},{geslo}\n')
+    #slovar_gesel[ime_app] = [uporabnisko_ime, email, geslo]
     #nastavijo zgornje ikkone nazaj na prazno
     dlg.leImeappa.setText('')
     dlg.leUsername.setText('')
     dlg.leEmail.setText('')
     dlg.leGeslo.setText('')
+
+    #if ime_app != '':
+     #   dlg.label_5.setText(f'{dlg.label_5.text()}\n {ime_app}')
 
     if ime_app != '':
         dlg.label_5.setText(f'{dlg.label_5.text()}\n {ime_app}')
@@ -36,6 +41,7 @@ def povej_podatke():
 
 
     if ime_appa2 in slovar_gesel:
+
         up_ime, mejl, passw = slovar_gesel[ime_appa2]
         dlg.lbVrnjeniPodatki.setText(
             f'Ime aplikacije: {ime_appa2}\nUporabniško ime: {up_ime}\nEmail: {mejl},\nGeslo: {passw}')
@@ -46,8 +52,37 @@ def povej_podatke():
 
 
 
+
+def povej_podatke_proba():
+    ime_appa2 = dlg.leImeappa2.text()
+
+    with open('gesla.csv', 'r', encoding='utf-8') as datoteka:
+        for vrstica in datoteka.read().splitlines():
+            if vrstica and ime_appa2 == vrstica.split(',')[0]:
+                ime_appa2, up_ime, mejl, passw = vrstica.split(',')
+                dlg.lbVrnjeniPodatki.setText(
+                    f'Ime aplikacije: {ime_appa2}\nUporabniško ime: {up_ime}\nEmail: {mejl},\nGeslo: {passw}')
+                break
+            elif not ime_appa2 in slovar_gesel:
+                dlg.lbVrnjeniPodatki.setText(
+                    f'Pod {ime_appa2} ni\n shranjene nobene aplikacije.')
+
+    #if ime_appa2 in slovar_gesel:
+
+     #   up_ime, mejl, passw = slovar_gesel[ime_appa2]
+      #  dlg.lbVrnjeniPodatki.setText(
+       #     f'Ime aplikacije: {ime_appa2}\nUporabniško ime: {up_ime}\nEmail: {mejl},\nGeslo: {passw}')
+
+    #elif not ime_appa2 in slovar_gesel:
+     #   dlg.lbVrnjeniPodatki.setText(
+      #      f'Pod {ime_appa2} ni\n shranjene nobene aplikacije.')
+
+
+
+
+
 dlg.btSave.clicked.connect(shrani)
-dlg.btVrnigesla.clicked.connect(povej_podatke)
+dlg.btVrnigesla.clicked.connect(povej_podatke_proba)
 
 dlg.show()
 app.exec()
